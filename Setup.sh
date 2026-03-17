b4ab43fb6354ec74b01df8a63986cae3c33294bb
willbill0808 <williamlrosewell08@gmail.com>
2026-03-17 16:42:22 +0100

    Update package installation commands in Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -4,23 +4,21 @@ cd ~
 
 sudo -v
 
-sudo sed -i '/#\[multilib\]/,/Include/s/^#//' /etc/pacman.conf
-
-sudo pacman -Syu nwg-look otf-geist-mono-nerd impala nwg-displays --noconfirm
-
 
 #ordered installs
 
-sudo pacman -Suy
+sudo sed -i '/#\[multilib\]/,/Include/s/^#//' /etc/pacman.conf
+
+sudo pacman -Suy --noconfirm
 echo "updated"
 
-sudo pacman -S zsh base-devel git gnome-keyring hyprlock hypridle hyprpaper swww unzip nautilus vscode rofi fastfetch btop flatpak impala swaync --noconfirm
+sudo pacman -S zsh base-devel git gnome-keyring hyprlock hypridle hyprpaper swww unzip nautilus vscode rofi fastfetch btop flatpak impala swaync nwg-displays --noconfirm
 echo "installed important packages"
 
-sudo pacman -S steam discord cava spotify-launcher libreoffice-fresh --noconfirm
+sudo pacman -S steam discord cava spotify-launcher libreoffice-fresh nwg-look imagemagick--noconfirm
 echo "installed extra packages"
 
-sudo pacman -S libreoffice-fresh-nr ttf-firacode-nerd noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ttf-dejavu ttf-liberation --noconfirm
+sudo pacman -S libreoffice-fresh-nr ttf-firacode-nerd noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ttf-dejavu ttf-liberation otf-geist-mono-nerd --noconfirm
 echo "installed language and font packages"
 fc-cache -rv
 
